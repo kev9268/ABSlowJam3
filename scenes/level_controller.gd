0ee368975e19cb604deced7ea1b6e1b4b3f33ebc
coloredasterisk <108952303+coloredasterisk@users.noreply.github.com>
2025-05-26 01:21:29 -0600

    mouse visual (#20)
    -fixed bug with cloud turning to branch by being pushed
    -add cursor held by a level store
    -expand branch on click, might be abused though only if with unlimited branches though

---
+++ scenes/level_controller.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var cursor : Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cursor = $Cursor
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	cursor_follow()
+
+func cursor_follow():
+	cursor.global_position = get_local_mouse_position()
