99bccd4e26c7fa7ee393d7ebb889c76cceced526
coloredasterisk <108952303+coloredasterisk@users.noreply.github.com>
2025-06-06 00:23:29 -0600

    audio done (#38)
    * audio done
    
    * bug fix

--- scenes/level_controller.gd
+++ scenes/level_controller.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var cursor : Sprite2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cursor = $Cursor
@@ -13,3 +12,15 @@ func _process(delta: float) -> void:
 
 func cursor_follow():
 	cursor.global_position = get_local_mouse_position()
+
+
+func play_sound(sound_name : String):
+	if sound_name == "click":
+		$Audio/ClickSoundFX.play()
+	elif sound_name == "undo":
+		$Audio/UndoSoundFX.play()
+	elif sound_name == "collect":
+		$Audio/CollectSoundFX.play()
+	elif sound_name == "win":
+		$Audio/WinSoundFX.play()
+	
